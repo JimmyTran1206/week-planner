0d148f874d083e60f4e78e0d307a5fc8a271c84c
Kevin Li <likevinhp@gmail.com>
2023-06-02 20:43:16 +0000

    kevin

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // Input data saved here
-const globalData={
-  entry:[];
-}
-///////////////////////////
+const globalData = {
+  entry: []
+};
+/// ////////////////////////
 const $submitButton = document.querySelector('.submit');
 const $modalContainer = document.querySelector('.modal-container');
 const $addEntryButton = document.querySelector('.add-entry');
-const $form=document.querySelector('form.add-entry-form')
+const $form = document.querySelector('form.add-entry-form');
 
 $addEntryButton.addEventListener('click', function () {
   $modalContainer.classList.remove('hidden');
@@ -16,5 +16,13 @@ $submitButton.addEventListener('click', function (event) {
   event.preventDefault();
   $modalContainer.classList.add('hidden');
 
+});
+
+const $dayButtons = document.querySelector('.row day-buttons');
+
+$dayButtons.addEventListener('click', () => {
+  if (event.target.className === 'sunday') {
+
+  }
 
 });
